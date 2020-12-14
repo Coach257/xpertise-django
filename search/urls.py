c1b3785935672a4a203b9c94ae290137d272eb06
Yinghao Zhu <zhuyinghao1999@msn.cn>
2020-12-14 14:40:58 +0800

    chores: modify response attributes

--- search/urls.py
+++ search/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns =[
     path('',views.index,name='index'),
-    path('api/search_author',views.search_author,name='search_author'),
-    path('api/search_pubs',views.search_pubs,name='search_pubs')
+    path('api/v1/search_author',views.search_author,name='search_author'),
+    path('api/v1/search_paper',views.search_paper,name='search_paper')
 ]
--- search/views.py
+++ search/views.py
@@ -9,35 +9,23 @@ def index(request):
 
 # 搜索作者，返回作者的个人信息
 def search_author(request):
-    # author_name = request.POST['author_name']
-    author_name = 'Mark'
+    author_name = request.POST['author_name']
     author = next(scholarly.search_author(author_name))
     res = {'affiliation': author.affiliation,
-           'citedby': author.citedby,
+           #'citedby': author.citedby,
            'email': author.email,
-           'filled': author.filled,
+           #'filled': author.filled,
            'id': author.id,
            'interests': author.interests,
            'name': author.name,
            'url_picture': author.url_picture}
     return HttpResponse(json.dumps(res), content_type="application/json")
 
-# 搜索文献（可多篇，指定个数）
-def search_pubs(request):
-    # pub_name = request.POST['pub_name'] # 文献名
-    # num = int(request.POST['num']) # 希望检索多少篇文献
-    pub_name = 'Math'
-    num = 3
-    i=0
-    res=[]
-    for pub in scholarly.search_pubs(pub_name):
-        if(i==num): break 
-        content = {'bib': pub.bib,
-            'citations_link': pub.citations_link,
-            'filled': pub.filled,
-            'source': pub.source,
-            'url_add_sclib': pub.url_add_sclib,
-            'url_scholarbib': pub.url_scholarbib}
-        res.append(content)
-        i+=1
-    return HttpResponse(json.dumps(res), content_type="application/json")
+def search_paper(request):
+    paper_name = request.POST['paper_name'] # 文献名
+    paper=next(scholarly.search_pubs(paper_name))
+    content = {
+        'url':paper.bib['url'],
+        'venue': paper.bib['venue'],
+        'abstract': paper.bib['abstract']}
+    return HttpResponse(json.dumps(content), content_type="application/json")
